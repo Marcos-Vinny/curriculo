7539d2ce555735d0b1b64b7db1d1195546e6344e
Marcos-Vinny <vinnymarcos2006@gmail.com>
2025-01-24 09:50:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Animações podem ser adicionadas aqui.
+  // Exemplo: Adicionar um efeito de destaque ao passar o mouse por cima de uma seção
+});
