be5a6cea4ae113dd724aa9e2ed6b3fe6434983aa
Marcos-Vinny <vinnymarcos2006@gmail.com>
2025-02-10 22:48:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Animações podem ser adicionadas aqui.
-  // Exemplo: Adicionar um efeito de destaque ao passar o mouse por cima de uma seção
+  
 });
